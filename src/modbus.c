bf2dc2a3b7eb9313d4ee51481411225bc7983b10
Dimonix <Late2003@mail.ru>
2017-10-22 13:26:21 +0300

    Remove a duplicated semi-colon
    Found with the help of clang-format (from another branch)

--- src/modbus.c
+++ src/modbus.c
@@ -22,6 +22,7 @@
 
 #include "modbus.h"
 #include "modbus-private.h"
+#include "modbus-tcp-private.h"
 
 /* Internal use */
 #define MSG_LENGTH_UNDEFINED -1
@@ -207,9 +208,8 @@ static int send_msg(modbus_t *ctx, uint8_t *msg, int msg_length)
     return rc;
 }
 
-int modbus_send_raw_request(modbus_t *ctx, uint8_t *raw_req, int raw_req_length)
-{
-    sft_t sft;
+int modbus_send_raw_request(modbus_t *ctx, uint8_t *raw_req, int raw_req_length) {
+//    sft_t sft;
     uint8_t req[MAX_MESSAGE_LENGTH];
     int req_length;
 
@@ -226,12 +226,12 @@ int modbus_send_raw_request(modbus_t *ctx, uint8_t *raw_req, int raw_req_length)
         return -1;
     }
 
-    sft.slave = raw_req[0];
-    sft.function = raw_req[1];
-    /* The t_id is left to zero */
-    sft.t_id = 0;
+//    sft.slave = raw_req[0];
+//    sft.function = raw_req[1];
+//    /* The t_id is left to zero */
+//    sft.t_id = 0;
     /* This response function only set the header so it's convenient here */
-    req_length = ctx->backend->build_response_basis(&sft, req);
+    req_length = ctx->backend->build_request_basis(ctx, raw_req[1], 0, 0, req);
 
     if (raw_req_length > 2) {
         /* Copy data after function code */
@@ -240,6 +240,45 @@ int modbus_send_raw_request(modbus_t *ctx, uint8_t *raw_req, int raw_req_length)
     }
 
     return send_msg(ctx, req, req_length);
+}
+
+int modbus_send_raw_response(modbus_t *ctx, uint8_t *raw_rsp, int raw_rsp_length) {
+    sft_t sft;
+    uint8_t rsp[MAX_MESSAGE_LENGTH];
+    int rsp_length;
+
+    if (ctx == NULL) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    if (raw_rsp_length < 2 || raw_rsp_length > (MODBUS_MAX_PDU_LENGTH + 1)) {
+        /* The raw response must contain function and slave at least and
+           must not be longer than the maximum pdu length plus the slave
+           address. */
+        errno = EINVAL;
+        return -1;
+    }
+
+
+    sft.slave = raw_rsp[0];
+    sft.function = raw_rsp[1];
+    /* The t_id is left to zero */
+    if(ctx->backend->backend_type == _MODBUS_BACKEND_TYPE_TCP) {
+        modbus_tcp_t *ctx_tcp = ctx->backend_data;
+        sft.t_id = ctx_tcp->t_id;
+    } else
+        sft.t_id = 0;
+    /* This response function only set the header so it's convenient here */
+    rsp_length = ctx->backend->build_response_basis(&sft, rsp);
+
+    if (raw_rsp_length > 2) {
+        /* Copy data after function code */
+        memcpy(rsp + rsp_length, raw_rsp + 2, raw_rsp_length - 2);
+        rsp_length += raw_rsp_length - 2;
+    }
+
+    return send_msg(ctx, rsp, rsp_length);
 }
 
 /*
